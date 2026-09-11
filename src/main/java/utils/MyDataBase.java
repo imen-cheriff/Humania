61a9eced97281ee5924463634d38ae50f9be9b1e
Imen Cherif <imencherif510@gmail.com>
2026-09-11 21:52:01 +0100

    refactor(db): load DB credentials from application.properties

--- src/main/java/utils/MyDataBase.java
+++ src/main/java/utils/MyDataBase.java
@@ -6,23 +6,28 @@ import java.sql.SQLException;
 
 public class MyDataBase {
 
-    private static MyDataBase instance ;
-    final String URL ="jdbc:mysql://127.0.0.1:3306/humania_full";
-    final String USERNAME ="root";
-    final String PASSWORD ="";
-    private Connection cnx ;
-    private MyDataBase(){
-        try {
-            cnx = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    private static MyDataBase instance;
 
+    private final String URL;
+    private final String USERNAME;
+    private final String PASSWORD;
+
+    private Connection cnx;
+
+    private MyDataBase() {
+        this.URL = PropertiesUtil.getProperty("db.url");
+        this.USERNAME = PropertiesUtil.getProperty("db.username");
+        this.PASSWORD = PropertiesUtil.getProperty("db.password");
+
+        try {
+            cnx = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Connected .... ");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-
-    public static MyDataBase getInstance(){
+    public static MyDataBase getInstance() {
         if (instance == null)
             instance = new MyDataBase();
         return instance;
